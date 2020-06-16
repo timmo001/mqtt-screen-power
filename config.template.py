84ab246688b8c51b5ecbad0dcc46dcb41f853e78
tikismoke <webmaster@poudot.fr>
2020-06-16 19:49:38 +0200

    Add template for non-rpi devices (#2)
    * allow not only pi hdmi to turn on off
    
    * Formatting
    
    Co-authored-by: Aidan Timson <contact@timmo.xyz>

--- config.template.py
+++ config.template.py
@@ -3,5 +3,9 @@ mqtt_port = 1883
 mqtt_topic = "screen/rpi"
 mqtt_username = "USERNAME"
 mqtt_password = "PASSWORD"
+# Raspberry Pi
 power_on_command = "vcgencmd display_power 1"
 power_off_command = "vcgencmd display_power 0"
+# Other HDMI linux devices
+# power_on_command = "xset -display :0 dpms force on"
+# power_off_command = "xset -display :0 dpms force off"
